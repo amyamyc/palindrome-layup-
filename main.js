17e73e1d007cb62f0bc50940ff05c68929921750
Amy Chew <amychew@Amys-MacBook-Pro.local>
2018-02-22 21:54:31 -0800

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const name = hannah
+
+if ( name.split('').join() ==== name.split('').reverse()
+
+// split hannah into individual letters.
+// Reverse the spelling of hannah
+// If reversed Hannah is the same as original Hannah then the name Hannah is a
+// a palindrome.
+
+const isPalindrome (hannah)
